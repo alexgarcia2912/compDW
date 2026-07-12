adc04c632c238bd9ee0df4698a51b836006242d2
alexgarcia2912 <neiradiego63@gmail.com>
2026-07-12 02:20:07 -0500

    modificar cors para netlify y cualquier ruta

--- BACK/src/server.js
+++ BACK/src/server.js
@@ -11,11 +11,11 @@ dotenv.config()
 const app = express()
 
 app.use(cors({
-    origin: '*', 
+    origin: ['http://localhost:5173', 'https://netlify.app'],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization']
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true
 }))
-
 
 app.use(express.json())
 app.use(cookieParser())
